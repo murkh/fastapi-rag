f45b5c192426001642f176e486d73c022b91d54c
Gaurav Panchal <gkp.125@gmail.com>
2025-05-25 16:26:05 +0530

    feat: Add UUIDMixin and TimestampMixin for model attributes

---
+++ src/app/core/db/models.py
@@ -0,0 +1,19 @@
+
+from datetime import UTC, datetime
+import uuid as uuid_mod
+from sqlalchemy import Column, DateTime, text
+
+from sqlalchemy.dialects.postgresql import UUID
+
+
+class UUIDMixin:
+    uuid: uuid_mod.UUID = Column(
+        UUID, primary_key=True, default=uuid_mod.uuid4, server_default=text("gen_random_uuid()")
+    )
+
+
+class TimestampMixin:
+    created_at: datetime = Column(DateTime, default=datetime.now(
+        UTC), server_default=text("current_timestamp(0)"))
+    updated_at: datetime = Column(DateTime, nullable=True, onupdate=datetime.now(
+        UTC), server_default=text("current_timestamp(0)"))
